c1c2c203ddef7ea1d42dbca4cf979166d71aefc9
Jeremiah Yan <jeremiah.yxj@gmail.com>
2017-12-28 22:11:15 +0800

    [U]Updated Travis Config

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@ echo $PATH
 
 cd /home/apps/go/src/github.com/AzureRelease/boiler-server && go build
 
-cd /home/apps/go/src/github.com/AzureRelease/boiler-server && nohup ./boiler-server &
-
 rm -rf common
 rm -rf controllers
 rm -rf models
