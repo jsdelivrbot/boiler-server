04a772085b315a53fcce6b872f4d3f9b0d08b2a9
catyxj <245959575@qq.com>
2018-01-17 16:54:03 +0800

    datatable-history table modified, list status add tips

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -50,8 +50,7 @@ gulp.task('minCss',function(){
 gulp.task('jsBase', function () {
     gulp.src(['./assets/global/plugins/jquery.min.js','./assets/global/plugins/bootstrap/js/bootstrap.min.js',
         './assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js',
-        './assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js',
-        './assets/global/plugins/jquery.blockui.min.js',
+
         './assets/global/plugins/js.cookie.min.js',
         './assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
         './assets/global/plugins/angularjs/angular.min.js',
@@ -72,9 +71,7 @@ gulp.task('jsPlugins', function () {
     gulp.src(['bower_components/moment/moment.js','bower_components/moment/locale/zh-cn.js',
         'bower_components/angular-moment/angular-moment.min.js',
         'bower_components/angular-ui-select/dist/select.js',
-        './assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js',
-        './assets/global/plugins/fullcalendar/fullcalendar.min.js',
-        './assets/global/plugins/horizontal-timeline/horizontal-timeline.js',
+
         './assets/global/plugins/datatables/datatables.min.js',
         'bower_components/sweetalert2/dist/sweetalert2.min.js',
         'bower_components/angular-bootstrap-switch/dist/angular-bootstrap-switch.min.js',
@@ -89,6 +86,16 @@ gulp.task('jsPlugins', function () {
         .pipe(uglify())
         .pipe(gulp.dest('js'));
 });
+
+gulp.task('jsAmcharts', function () {
+    gulp.src([
+
+    ])
+        .pipe(concat('amcharts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'));
+});
+
 
 gulp.task('jsLayouts', function () {
     gulp.src(['js/app.js','./assets/layouts/boiler/scripts/layout.min.js',
