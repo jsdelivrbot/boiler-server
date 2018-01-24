011feb8e7e0196ad7e2503e92c5989b7ea9a808b
Jeremiah Yan <jeremiah.yxj@gmail.com>
2018-01-24 16:22:49 +0800

    [U]Updated Preview

--- controllers/parameter_controller.go
+++ controllers/parameter_controller.go
@@ -966,7 +966,7 @@ func generateDefaultChannelConfig() error {
 }
 
 func (ctl *ParameterController) InitParameterChannelConfig(limit int64) {
-	//generateDefaultChannelConfig()
+	// generateDefaultChannelConfig()
 	ParamCtrl.ReloadLimit = limit
 
 	interval := time.Second * 15
