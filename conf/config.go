534b858a1ea76ddde38ad99b14aa2bdca2258654
Jeremiah Yan <jeremiah.yxj@gmail.com>
2018-01-31 04:35:28 +0800

    [Pr]Product Release

--- conf/config.go
+++ conf/config.go
@@ -2,5 +2,5 @@ package conf
 
 import "github.com/AzureTech/goazure"
 
-var IsRelease bool = (goazure.AppConfig.String("runmode") == "prod")
+var IsRelease bool = goazure.AppConfig.String("runmode") == "prod"
 var Version string = "e884cb938479205f9e25c91152bf359db1c35d6f"
