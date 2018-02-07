f30e9d168a927638edfe0655c54917f68974fd11
Xumbra <364748193@qq.com>
2018-02-07 10:49:20 +0800

    Update mysql.go

--- dba/mysql.go
+++ dba/mysql.go
@@ -10,7 +10,6 @@ import (
 	"net/url"
 	"github.com/AzureRelease/boiler-server/common"
 
-	"github.com/AzureRelease/boiler-server/conf"
 )
 
 var MyORM 		orm.Ormer
@@ -20,13 +19,7 @@ func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
 
-	var myConnection string
-
-	if conf.IsRelease {
-		myConnection = "azureadmin:azure%2016@tcp(rm-a0z2ur23e09te04c8h4.mysql.rds.aliyuncs.com:3306)/boiler_main?charset=utf8&loc=" + url.QueryEscape("PRC")
-	} else {
-		myConnection = "azureadmin:azure%2016@tcp(rm-a0z2ur23e09te04c8h4n.mysql.rds.aliyuncs.com:3306)/boiler_main?charset=utf8&loc=" + url.QueryEscape("PRC")
-	}
+	var myConnection string = "root:hold2017@tcp(47.100.0.27:3306)/boiler?charset=utf8&loc=" + url.QueryEscape("PRC")
 
 
 
