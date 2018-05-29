39e17003f0c786a172ca42ef17b2999880b50da7
df <kdfmail@qq.com>
2018-05-29 16:58:48 +0800

    change organization update info

--- models/boiler_calculate_parameter.go
+++ models/boiler_calculate_parameter.go
@@ -60,19 +60,19 @@ func init() {
 	DefaultCalcParam.CoalQnetvar = 20710
 	DefaultCalcParam.CoalAar = 7.92
 	DefaultCalcParam.CoalClz = 43.5
-	DefaultCalcParam.CoalClm = 31.66
-	DefaultCalcParam.CoalCfh = 16.08
-	DefaultCalcParam.CoalAlz = 75
-	DefaultCalcParam.CoalAlm = 5
-	DefaultCalcParam.CoalAfh = 20
-	DefaultCalcParam.CoalQ3 = 0.5
-	DefaultCalcParam.CoalM = 0.4
-	DefaultCalcParam.CoalN = 3.6
-	DefaultCalcParam.CoalCtLz = 570.2
+DefaultCalcParam.CoalClm = 31.66
+DefaultCalcParam.CoalCfh = 16.08
+DefaultCalcParam.CoalAlz = 75
+DefaultCalcParam.CoalAlm = 5
+DefaultCalcParam.CoalAfh = 20
+DefaultCalcParam.CoalQ3 = 0.5
+DefaultCalcParam.CoalM = 0.4
+DefaultCalcParam.CoalN = 3.6
+DefaultCalcParam.CoalCtLz = 570.2
 
-	DefaultCalcParam.GasQ3 = 0.5
-	DefaultCalcParam.GasM = 0.4
-	DefaultCalcParam.GasN = 3.6
+DefaultCalcParam.GasQ3 = 0.5
+DefaultCalcParam.GasM = 0.4
+DefaultCalcParam.GasN = 3.6
 
-	DefaultCalcParam.ConfParam1 = 2.9
+DefaultCalcParam.ConfParam1 = 2.9
 }
