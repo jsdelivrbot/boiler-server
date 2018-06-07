3d6a7a15209c47d1b58426d58ee5b66a71fc2a5a
df <kdfmail@qq.com>
2018-06-07 17:30:03 +0800

    update fast channel config

--- controllers/fast_config_controller.go
+++ controllers/fast_config_controller.go
@@ -7,6 +7,10 @@ import (
 	"encoding/json"
 	"github.com/AzureRelease/boiler-server/dba"
 	"fmt"
+	"strconv"
+	"github.com/pborman/uuid"
+	"sort"
+	"github.com/AzureTech/goazure/orm"
 )
 
 type FastConfigController struct {
@@ -16,6 +20,75 @@ type FastConfigController struct {
 type termCombined struct {
 	BoilerUid  string    `json:"boiler_uid"`
 	Code       int64    `json:"code"`
+}
+
+type FastChannelConfig struct {
+	Chan  Chans				`json:"Chan"`
+	Param CommParam			`json:"Param"`
+	Code  int  `json:"Code"`
+}
+
+type Chans struct {
+   Analogue  []Analogue	`json:"Analogue"`
+   Switch    []Switch	`json:"Switch"`
+   Range    []Range		`json:"Range"`
+}
+
+type Analogue struct {
+	Parameter Parameter		`json:"Parameter"`
+	Func int		`json:"Func"`
+	Byte int		`json:"Byte"`
+	Modbus int		`json:"Modbus"`
+	ChannelNumber int		`json:"ChannelNumber"`
+	SequenceNumber int32		`json:"SequenceNumber"`
+	Status int32			`json:"Status"`
+	SwitchStatus int32		`json:"SwitchStatus"`
+}
+
+type Switch struct {
+	Parameter Parameter		`json:"Parameter"`
+	Func int			`json:"Func"`
+	Modbus int           `json:"Modbus"`
+	BitAddress int 		`json:"BitAddress"`
+	ChannelNumber int		`json:"ChannelNumber"`
+	SequenceNumber int32		`json:"SequenceNumber"`
+	Status int32			`json:"Status"`
+	SwitchStatus int32		`json:"SwitchStatus"`
+}
+
+type Range struct {
+	Parameter Parameter  	`json:"Parameter"`
+	Func int 		`json:"Func"`
+	Byte int			`json:"Byte"`
+	Modbus int			`json:"Modbus"`
+	Ranges []Ranges		`json:"Ranges"`
+	ChannelNumber  int  `json:"ChannelNumber"`
+	SequenceNumber int32  `json:"SequenceNumber"`
+	Status   int32        `json:"Status"`
+	SwitchStatus  int32   `json:"SwitchStatus"`
+
+}
+
+type Parameter struct {
+	Name string   `json:"Name"`
+	Scale float32	`json:"Scale"`
+	Unit string		`json:"Unit"`
+}
+
+type Ranges struct {
+	Min int64		`json:"Min"`
+	Max int64		`json:"Max"`
+	Name string     `json:"Name"`
+}
+
+type CommParam struct {
+	BaudRate int	`json:"BaudRate"`
+	DataBit int		`json:"DataBit"`
+	StopBit int		`json:"StopBit"`
+	CheckDigit int  `json:"CheckDigit"`
+	CommunInterface int  `json:"CommunInterface"`
+	SlaveAddress int   `json:"SlaveAddress"`
+	HeartBeat int   `json:"HeartBeat"`
 }
 
 func (ctl *FastConfigController) FastBoilerAdd() {
@@ -147,13 +220,23 @@ func (ctl *FastConfigController) FastTermCombined() {
 		ctl.Ctx.Output.Body([]byte("终端已被占用!"))
 		return
 	}
-
-	sql:="insert into boiler_terminal_combined(boiler_id,terminal_id,terminal_code,terminal_set_id) values(?,?,?,?) on duplicate key update terminal_id=?,terminal_code = ?"
-	if _,err:=dba.BoilerOrm.Raw(sql,wCombined.BoilerUid,terminal.Uid,terminal.TerminalCode,1,terminal.Uid,terminal.TerminalCode).Exec();err!=nil{
-		goazure.Error("Insert boiler_terminal_combined Error",err)
-		ctl.Ctx.Output.SetStatus(400)
-		ctl.Ctx.Output.Body([]byte("终端绑定失败"))
-		return
+	var sql string =""
+	if dba.BoilerOrm.QueryTable("boiler_terminal_combined").Filter("Boiler__Uid",wCombined.BoilerUid).Filter("TerminalSetId",1).Exist() {
+		sql="update boiler_terminal_combined set terminal_id= ? ,terminal_code = ? where boiler_id=? and terminal_set_id = 1"
+		if _,err:=dba.BoilerOrm.Raw(sql,terminal.Uid,terminal.TerminalCode,wCombined.BoilerUid).Exec();err!=nil{
+			goazure.Error("Update boiler_terminal_combined Error",err)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte("终端绑定失败"))
+			return
+		}
+	} else {
+		sql ="insert into boiler_terminal_combined(boiler_id,terminal_id,terminal_code,terminal_set_id) values(?,?,?,?)"
+		if _,err:=dba.BoilerOrm.Raw(sql,wCombined.BoilerUid,terminal.Uid,terminal.TerminalCode,1).Exec();err!=nil{
+			goazure.Error("Insert boiler_terminal_combined Error",err)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte("终端绑定失败"))
+			return
+		}
 	}
 }
 
@@ -179,9 +262,293 @@ func (ctl *FastConfigController) FastTermUnbind() {
 		goazure.Error("Unbind Boiler/Terminal Error",err)
 		ctl.Ctx.Output.SetStatus(400)
 		ctl.Ctx.Output.Body([]byte("解绑失败"))
+		return
 	}
 }
 
 func (ctl *FastConfigController) FastTermChannelConfig() {
+	var config FastChannelConfig
 
+	if err := json.Unmarshal(ctl.Ctx.Input.RequestBody, &config); err != nil {
+		goazure.Error("Unmarshal JSON Error", err)
+	}
+	var terminal models.Terminal
+	if err := dba.BoilerOrm.QueryTable("Terminal").Filter("TerminalCode", config.Code).One(&terminal); err != nil {
+		goazure.Error("Query Terminal Error:", err)
+		ctl.Ctx.Output.SetStatus(400)
+		ctl.Ctx.Output.Body([]byte("找不到终端"))
+		return
+	}
+	for _, analogue := range config.Chan.Analogue {
+		var param models.RuntimeParameter
+		var cnf models.RuntimeParameterChannelConfig
+		var max int32
+		sql := "select max(param_id) as param_id from runtime_parameter where category_id=? and is_deleted =false"
+		if err := dba.BoilerOrm.Raw(sql, models.RUNTIME_PARAMETER_CATEGORY_ANALOG).QueryRow(&max); err != nil {
+			goazure.Error("Query runtime_parameter Error", err)
+		}
+		if max >= 100 {
+			param.ParamId = max + 1
+		} else {
+			param.ParamId = 100
+		}
+		a := fmt.Sprintf("%d%d", models.RUNTIME_PARAMETER_CATEGORY_ANALOG, param.ParamId)
+		param.Id, _ = strconv.ParseInt(a, 10, 64)
+		param.Length = 2
+		param.Fix = 2
+		param.Category = runtimeParameterCategory(models.RUNTIME_PARAMETER_CATEGORY_ANALOG)
+		param.Name = analogue.Parameter.Name
+		param.Unit = analogue.Parameter.Unit
+		param.Scale = analogue.Parameter.Scale
+		param.Medium = runtimeParameterMedium(0)
+		param.AddBoilerMedium(0)
+		param.Organization = ctl.GetCurrentUser().Organization
+		param.CreatedBy = ctl.GetCurrentUser()
+		param.UpdatedBy = ctl.GetCurrentUser()
+		param.IsDeleted = false
+		if err := DataCtl.AddData(&param, true); err != nil {
+			e := fmt.Sprintln("Add/Update Parameter Error", err)
+			goazure.Error(e)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte(e))
+			return
+		}
+		cnf.Uid = uuid.New()
+		cnf.Terminal = &terminal
+		cnf.ChannelType = int32(models.CHANNEL_TYPE_TEMPERATURE)
+		cnf.ChannelNumber = int32(analogue.ChannelNumber)
+		cnf.IsDefault = false
+
+		//TODO: Default Signed & Threshold
+		cnf.Signed = true
+		cnf.NegativeThreshold = 32768
+		cnf.Parameter = &param
+		cnf.Status = analogue.Status
+		if cnf.Status == models.CHANNEL_STATUS_SHOW {
+			cnf.SequenceNumber = analogue.SequenceNumber
+		} else {
+			cnf.SequenceNumber = -1
+		}
+
+		cnf.Name = cnf.Parameter.Name
+		cnf.Length = cnf.Parameter.Length
+		if _, err := dba.BoilerOrm.Insert(&cnf); err != nil {
+			goazure.Error("Insert Analogue Error:", err)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte("添加模拟量失败"))
+			return
+		}
+		analoguesql := "insert into issued_analogue_switch(channel_id,create_time,function_id,byte_id,modbus,bit_address) values(?,now(),?,?,?,?)"
+		if _, err := dba.BoilerOrm.Raw(analoguesql, cnf.Uid, analogue.Func, analogue.Byte, analogue.Modbus, 0).Exec(); err != nil {
+			goazure.Error("Insert issued_analogue Error", err)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte("添加模拟量功能码等失败"))
+			return
+		}
+	}
+	for _, swi := range config.Chan.Switch {
+		var max int32
+		var param models.RuntimeParameter
+		var cnf models.RuntimeParameterChannelConfig
+		sql := "select max(param_id) as param_id from runtime_parameter where category_id=? and is_deleted =false"
+		if err := dba.BoilerOrm.Raw(sql, models.RUNTIME_PARAMETER_CATEGORY_SWITCH).QueryRow(&max); err != nil {
+			goazure.Error("Query runtime_parameter Error", err)
+		}
+		if max >= 100 {
+			param.ParamId = max + 1
+		} else {
+			param.ParamId = 100
+		}
+		a := fmt.Sprintf("%d%d", models.RUNTIME_PARAMETER_CATEGORY_SWITCH, param.ParamId)
+		param.Id, _ = strconv.ParseInt(a, 10, 64)
+		param.Length = 1
+		param.Fix = 0
+		param.Category = runtimeParameterCategory(models.RUNTIME_PARAMETER_CATEGORY_SWITCH)
+		param.Name = swi.Parameter.Name
+		//param.Unit = ""
+		param.Scale = 1
+		param.Medium = runtimeParameterMedium(0)
+		param.AddBoilerMedium(0)
+		param.Organization = ctl.GetCurrentUser().Organization
+		param.CreatedBy = ctl.GetCurrentUser()
+		param.UpdatedBy = ctl.GetCurrentUser()
+		param.IsDeleted = false
+		if err := DataCtl.AddData(&param, true); err != nil {
+			e := fmt.Sprintln("Add/Update Parameter Error", err)
+			goazure.Error(e)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte(e))
+			return
+		}
+		cnf.Uid = uuid.New()
+		cnf.Terminal = &terminal
+		cnf.ChannelType = int32(models.CHANNEL_TYPE_SWITCH)
+		cnf.ChannelNumber = int32(swi.ChannelNumber)
+		cnf.IsDefault = false
+
+		//TODO: Default Signed & Threshold
+		cnf.Signed = true
+		cnf.NegativeThreshold = 32768
+		cnf.Parameter = &param
+		cnf.Status = swi.Status
+		if cnf.Status == models.CHANNEL_STATUS_SHOW {
+			cnf.SequenceNumber = swi.SequenceNumber
+		} else {
+			cnf.SequenceNumber = -1
+		}
+		if swi.SwitchStatus == 0 {
+			swi.SwitchStatus = 1
+		}
+		cnf.SwitchStatus = swi.SwitchStatus
+
+		cnf.Name = cnf.Parameter.Name
+		cnf.Length = cnf.Parameter.Length
+		if cnf.ChannelNumber == 1 || cnf.ChannelNumber == 2 {
+			switchburnsql := "insert into issued_switch_default(uid,terminal_id,create_time,channel_type,channel_number,function_id,modbus,bit_address) values(uuid(),?,now(),?,?,?,?,?)"
+			if _, err := dba.BoilerOrm.Raw(switchburnsql, terminal.Uid, models.CHANNEL_TYPE_SWITCH, swi.ChannelNumber, swi.Func, swi.Modbus, swi.BitAddress).Exec(); err != nil {
+				goazure.Error("Insert issued_switch_default Error", err)
+				ctl.Ctx.Output.SetStatus(400)
+				ctl.Ctx.Output.Body([]byte("添加开关点火plc失败"))
+				return
+			}
+		} else {
+			if _, err := dba.BoilerOrm.Insert(&cnf); err != nil {
+				goazure.Error("Insert Switch Error:", err)
+				ctl.Ctx.Output.SetStatus(400)
+				ctl.Ctx.Output.Body([]byte("添加开关量失败"))
+				return
+			}
+			analoguesql := "insert into issued_analogue_switch(channel_id,create_time,function_id,byte_id,modbus,bit_address) values(?,now(),?,?,?,?)"
+			if _, err := dba.BoilerOrm.Raw(analoguesql, cnf.Uid, swi.Func, 0, swi.Modbus, swi.BitAddress).Exec(); err != nil {
+				goazure.Error("Insert issued_analogue Error", err)
+				ctl.Ctx.Output.SetStatus(400)
+				ctl.Ctx.Output.Body([]byte("添加模拟量功能码等失败"))
+				return
+			}
+		}
+	}
+	for _, ran := range config.Chan.Range {
+		var max int32
+		var param models.RuntimeParameter
+		var cnf models.RuntimeParameterChannelConfig
+		sql := "select max(param_id) as param_id from runtime_parameter where category_id=? and is_deleted =false"
+		if err := dba.BoilerOrm.Raw(sql, models.RUNTIME_PARAMETER_CATEGORY_RANGE).QueryRow(&max); err != nil {
+			goazure.Error("Query runtime_parameter Error", err)
+		}
+		if max >= 100 {
+			param.ParamId = max + 1
+		} else {
+			param.ParamId = 100
+		}
+		a := fmt.Sprintf("%d%d", models.RUNTIME_PARAMETER_CATEGORY_RANGE, param.ParamId)
+		param.Id, _ = strconv.ParseInt(a, 10, 64)
+		param.Length = 1
+		param.Fix = 0
+		param.Category = runtimeParameterCategory(models.RUNTIME_PARAMETER_CATEGORY_RANGE)
+		param.Name = ran.Parameter.Name
+		//param.Unit = ""
+		param.Scale = 1
+		param.Medium = runtimeParameterMedium(0)
+		param.AddBoilerMedium(0)
+		param.Organization = ctl.GetCurrentUser().Organization
+		param.CreatedBy = ctl.GetCurrentUser()
+		param.UpdatedBy = ctl.GetCurrentUser()
+		param.IsDeleted = false
+		if err := DataCtl.AddData(&param, true); err != nil {
+			e := fmt.Sprintln("Add/Update Parameter Error", err)
+			goazure.Error(e)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte(e))
+			return
+		}
+		cnf.Uid = uuid.New()
+		cnf.Terminal = &terminal
+		cnf.ChannelType = int32(models.CHANNEL_TYPE_RANGE)
+		cnf.ChannelNumber = int32(ran.ChannelNumber)
+		cnf.IsDefault = false
+
+		//TODO: Default Signed & Threshold
+		cnf.Signed = true
+		cnf.NegativeThreshold = 32768
+		cnf.Parameter = &param
+		cnf.Status = ran.Status
+		if cnf.Status == models.CHANNEL_STATUS_SHOW {
+			cnf.SequenceNumber = ran.SequenceNumber
+		} else {
+			cnf.SequenceNumber = -1
+		}
+
+		cnf.Name = cnf.Parameter.Name
+		cnf.Length = cnf.Parameter.Length
+		if _, err := dba.BoilerOrm.Insert(&cnf); err != nil {
+			goazure.Error("Insert Range Error:", err)
+			ctl.Ctx.Output.SetStatus(400)
+			ctl.Ctx.Output.Body([]byte("添加状态量失败"))
+			return
+		}
+
+		sort.Sort(ByRMin(ran.Ranges))
+		for i, r := range ran.Ranges {
+			var rg models.RuntimeParameterChannelConfigRange
+			if r.Max < r.Min {
+				e := fmt.Sprintln("状态值范围设定有误！")
+				goazure.Error(e)
+				ctl.Ctx.Output.SetStatus(400)
+				ctl.Ctx.Output.Body([]byte(e))
+				return
+			}
+			rg.ChannelConfig = &cnf
+			rg.Min = r.Min
+			rg.Max = r.Max
+			rg.Name = r.Name
+			rg.Value = int64(i)
+			if err := DataCtl.AddData(&rg, true); err != nil {
+				goazure.Error("Added ChannelConfig Range Error", rg, err)
+			}
+		}
+			analoguesql := "insert into issued_analogue_switch(channel_id,create_time,function_id,byte_id,modbus,bit_address) values(?,now(),?,?,?,?)"
+			if _, err := dba.BoilerOrm.Raw(analoguesql, cnf.Uid, ran.Func, ran.Byte, ran.Modbus, 0).Exec(); err != nil {
+				goazure.Error("Insert issued_analogue Error", err)
+				ctl.Ctx.Output.SetStatus(400)
+				ctl.Ctx.Output.Body([]byte("添加状态量功能码等失败"))
+				return
+			}
+	}
+	go ParamCtrl.RefreshParameters()
+	//插入表通信参数
+	sql := "replace into issued_communication(terminal_id,baud_rate_id,data_bit_id,stop_bit_id,check_bit_id,correspond_type_id,sub_address_id,heart_beat_id) values(?,?,?,?,?,?,?,?)"
+	if _, err := dba.BoilerOrm.Raw(sql, terminal.Uid, config.Param.BaudRate, config.Param.DataBit, config.Param.StopBit, config.Param.CheckDigit,config.Param.CommunInterface, config.Param.SlaveAddress,config.Param.HeartBeat).Exec(); err != nil {
+		goazure.Error("Insert issued_communication Error", err)
+		ctl.Ctx.Output.SetStatus(400)
+		ctl.Ctx.Output.Body([]byte("添加通信参数失败"))
+		return
+	}
+	var param []orm.Params
+	var ver int32
+	verSql := "select ver from issued_message where sn=?"
+	if num, err := dba.BoilerOrm.Raw(verSql, terminal.TerminalCode).Values(&param); err != nil || num == 0 {
+		goazure.Error("Query issued_message Error", err)
+		ver = 1
+	} else {
+		a := fmt.Sprintf("%s", param[0]["ver"])
+		v, err := strconv.Atoi(a)
+		ver = int32(v) + 1
+		if ver >= 32769 {
+			ver = 1
+		}
+		if err != nil {
+			goazure.Error("ParseInt Error")
+			return
+		}
+	}
+	Byte := IssuedCtl.IssuedMessage(terminal.Uid)
+	Code := strconv.FormatInt(terminal.TerminalCode, 10)
+	message := SocketCtrl.c0(Byte, Code, ver)
+	messageSql := "insert into issued_message(sn,ver,create_time,update_time,curr_message) values(?,?,now(),now(),?) on duplicate key update ver=?,update_time=now(),curr_message=?"
+	if _, err := dba.BoilerOrm.Raw(messageSql, terminal.TerminalCode, ver, string(message), ver, string(message)).Exec(); err != nil {
+		goazure.Error("Insert issued_message Error", err)
+		ctl.Ctx.Output.SetStatus(400)
+		ctl.Ctx.Output.Body([]byte("组包失败"))
+		return
+	}
 }
