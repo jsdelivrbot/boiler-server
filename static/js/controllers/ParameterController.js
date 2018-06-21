d6cb185a63d6c254a15dac93f554faf51134004d
catyxj <245959575@qq.com>
2018-06-21 20:07:58 +0800

    666

--- static/js/controllers/ParameterController.js
+++ static/js/controllers/ParameterController.js
@@ -94,8 +94,9 @@ angular.module('BoilerAdmin').controller('ParameterController', function ($rootS
         DTColumnDefBuilder.newColumnDef(1),
         DTColumnDefBuilder.newColumnDef(2),
         DTColumnDefBuilder.newColumnDef(3),
-        DTColumnDefBuilder.newColumnDef(4).notSortable(),
-        DTColumnDefBuilder.newColumnDef(5).notSortable()
+        DTColumnDefBuilder.newColumnDef(4),
+        DTColumnDefBuilder.newColumnDef(5).notSortable(),
+        DTColumnDefBuilder.newColumnDef(6).notSortable()
     ];
 
     bParameter.new = function () {
